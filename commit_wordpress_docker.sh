b34d464cb5ba2227056aadeb362a6caa20a91f0c
zhouzhipeng <admin@zhouzhipeng.com>
2018-02-08 10:03:25 +0800

    update

---
+++ commit_wordpress_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+tag=$1
+comment=$2
+
+ssh zhipeng.zhou@blog.zhouzhipeng.com "\
+docker  commit -m $comment wordpress  registry.cn-shanghai.aliyuncs.com/zhouzhipeng/wordpress:$tag  \ 
+&&  docker push  registry.cn-shanghai.aliyuncs.com/zhouzhipeng/wordpress:$tag \
+"

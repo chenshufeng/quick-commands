11325e2b63fd76b4bce1ad1b32f3ac70d66bbf52
zhouzhipeng <admin@zhouzhipeng.com>
2018-02-08 11:26:48 +0800

    脚本调整

--- commit_wordpress_docker.sh
+++ commit_wordpress_docker.sh
@@ -3,7 +3,8 @@
 tag=$1
 comment=$2
 
+echo "receive params:[$@]"
+
 ssh zhipeng.zhou@blog.zhouzhipeng.com "\
-docker  commit -m '${comment}' wordpress  registry.cn-shanghai.aliyuncs.com/zhouzhipeng/wordpress:${tag}  \ 
-&&  docker push  registry.cn-shanghai.aliyuncs.com/zhouzhipeng/wordpress:${tag} \
+docker  commit -m '${comment}' wordpress  registry.cn-shanghai.aliyuncs.com/zhouzhipeng/wordpress:${tag} &&  docker push  registry.cn-shanghai.aliyuncs.com/zhouzhipeng/wordpress:${tag} \
 "

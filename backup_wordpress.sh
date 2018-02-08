3a24954cf889c69b9003fe20cb27a8d287a752b1
周志鹏 <823143047@qq.com>
2018-02-08 16:37:07 +0800

    Update backup_wordpress.sh

--- backup_wordpress.sh
+++ backup_wordpress.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # 在自己本机上执行，先安装好对应wordpress的docker版mysql库
-docker exec -it  mysql5.7 bash -c "cd /wordpress-backup && mysqldump -h blog.zhouzhipeng.com -P 3307 -uroot -p wordpress > wordpress.sql"
+docker exec   mysql5.7 bash -c "cd /wordpress-backup && mysqldump -h blog.zhouzhipeng.com -P 3307 -uroot -p wordpress > wordpress.sql"

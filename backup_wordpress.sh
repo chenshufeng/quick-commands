d3e008044e32723b7ce665ecf0c09f6616b443b0
周志鹏 <823143047@qq.com>
2018-02-11 12:03:03 +0800

    Update and rename backup_wordpress.sh to backup_wordpress_mysql.sh

--- backup_wordpress.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# 在自己本机上执行，先安装好对应wordpress的docker版mysql库
-docker exec   mysql5.7 bash -c "cd /wordpress-backup && mysqldump -h blog.zhouzhipeng.com -P 3307 -uroot -p wordpress > wordpress.sql"

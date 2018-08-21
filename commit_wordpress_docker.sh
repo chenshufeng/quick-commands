5f01ef65a54d33bc695b938c289c53762a917c2f
周志鹏 <823143047@qq.com>
2018-08-21 15:27:22 +0800

    变更用户

--- commit_wordpress_docker.sh
+++ commit_wordpress_docker.sh
@@ -5,6 +5,6 @@ comment=$2
 
 echo "receive params:[$@]"
 
-ssh zhipeng.zhou@blog.zhouzhipeng.com "\
+ssh root@blog.zhouzhipeng.com "\
 docker  commit -m '${comment}' wordpress  registry.cn-shanghai.aliyuncs.com/zhouzhipeng/wordpress:${tag} &&  docker push  registry.cn-shanghai.aliyuncs.com/zhouzhipeng/wordpress:${tag} \
 "
